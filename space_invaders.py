e7067b4965955f762a8e4b274c972bb0d5e52166
jVsetecka <43730879+jVsetecka@users.noreply.github.com>
2020-05-10 22:29:03 +0200

    First commit

---
+++ space_invaders.py
@@ -0,0 +1,42 @@
+import pygame
+import math
+import random
+import tkinter as tk
+from tkinter import messagebox
+
+
+class Player:
+    def __init__(self, x, y, image):
+        pass
+
+    def move(self):
+        pass
+
+    def shoot(self):
+        pass
+
+
+class Enemy:
+    def __init__(self):
+        pass
+
+    def move(self):
+        pass
+
+    def shoot(self):
+        pass
+
+
+def create_window(width, height):
+    pass
+
+
+def is_collision(enemy_x, enemy_y, bullet_x, bullet_y):
+    pass
+
+
+def message_box(subject, content):
+    pass
+
+def main():
+    pass
